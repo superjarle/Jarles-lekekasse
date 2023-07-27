c7f4303a4829101373e37a856b7ee7686c8f72d4
Jarle Kvile <jkv@ssb.no>
2023-07-27 11:18:15 +0200

    Update app.R

--- Shiny/cobb-douglas-econ/app.R
+++ Shiny/cobb-douglas-econ/app.R
@@ -1,4 +1,3 @@
-#
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
@@ -10,9 +9,11 @@
 library(shiny)
 library(plotly)
 library(Deriv)
+library(shinythemes)
 
 C <- function(alpha, beta, w, r, q){
-  (((alpha/beta)^(beta/alpha) + (alpha/beta)^(-alpha/beta)) * w^(alpha/beta) * r^(beta/alpha) * q^(1/(alpha+beta)))
+  total = alpha + beta
+  (((alpha/beta)^(beta/total) + (alpha/beta)^(-alpha/total)) * w^(alpha/total) * r^(beta/total) * q^(1/total))
 }
 
 # Define the Cobb-Douglas production function
@@ -23,6 +24,8 @@ Cobb_Douglas <- function(A, alpha, beta, L, K){
 # Define UI
 ui <- fluidPage(
   
+  theme = shinytheme("united"), # Add a theme
+  
   # Application title
   titlePanel("Cobb-Douglas Cost and Production Functions"),
   
@@ -31,9 +34,9 @@ ui <- fluidPage(
     tabPanel("Cost Function", 
              sidebarLayout(
                sidebarPanel(
-                 h4("Cost Function Parameters"),
-                 p("Set the parameters below for the production function"),
-                 helpText("$$q=l^{\alpha}k^{\beta}$$"),
+                 h4("Cost Function Parameters", align = "center"),
+                 p("Set the parameters below for the production function", align = "center"),
+                 helpText("$$q=l^{\alpha}k^{\beta}$$", align = "center"),
                  sliderInput(inputId = "alpha_cost",
                              label = "Fixed Costs, alpha",
                              min = 0,
@@ -55,7 +58,7 @@ ui <- fluidPage(
                              max = 2.5,
                              value = 1.5, step=0.5),
                  sliderInput(inputId = "q",
-                             label = "Output, q",
+                             label = "Output quantity, q",
                              min = 0,
                              max = 100,
                              value = 50, step=5)
@@ -63,17 +66,18 @@ ui <- fluidPage(
                mainPanel(
                  plotlyOutput("cost_plot"),
                  p("Cost for q", uiOutput("cost_q"), align="center"),
+                 p("Alpha and Beta", uiOutput("alpha_beta_cost"), align="center"),
                  p("This model is coded by",
                    a("Jarle Kvile",
-                     href="https://github.com/superjarle"))
+                     href="https://github.com/superjarle"), align="center")
                )
              )),
     tabPanel("Production Function",
              sidebarLayout(
                sidebarPanel(
-                 h4("Production Function Parameters"),
-                 p("Set the parameters below for the production function"),
-                 helpText("$$Y=A*L^{\alpha}*K^{\beta}$$"),
+                 h4("Production Function Parameters", align = "center"),
+                 p("Set the parameters below for the production function", align = "center"),
+                 helpText("$$Y=A*L^{\alpha}*K^{\beta}$$", align = "center"),
                  sliderInput(inputId = "A",
                              label = "Total factor productivity, A",
                              min = 0,
@@ -101,8 +105,9 @@ ui <- fluidPage(
                              value = 50, step=5)
                ),
                mainPanel(
-                 plotlyOutput("prodity_plot"),
+                 plotlyOutput("utility_plot"),
                  p("Total Production (Y)", uiOutput("total_production"), align="center"),
+                 p("Returns to scale", uiOutput("returns_to_scale"), align="center"),
                  p("Alpha and Beta for Production Function", uiOutput("alpha_beta_prod"), align="center")
                )
              ))
@@ -111,6 +116,14 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  total_cost <- reactive({
+    input$alpha_cost + input$beta_cost
+  })
+  
+  total_prod <- reactive({
+    input$alpha_prod + input$beta_prod
+  })
   
   output$cost_plot <- renderPlotly({
     C_func <- function(q){C(input$alpha_cost, input$beta_cost, input$w, input$r, q)}
@@ -122,7 +135,7 @@ server <- function(input, output) {
              yaxis = list(title = "Costs"))
   })
   
-  output$prodity_plot <- renderPlotly({
+  output$utility_plot <- renderPlotly({
     Cobb_func <- function(L){Cobb_Douglas(input$A, input$alpha_prod, input$beta_prod, L, input$K)}
     df <- data.frame(x = seq(0,10, length.out = 1000))
     df$y <- Cobb_func(df$x)
@@ -133,27 +146,54 @@ server <- function(input, output) {
   })
   
   output$cost_q <- renderUI({
-    cost_val = C(input$alpha_cost, input$beta_cost, input$w, input$r, input$q)
-    cost_text <- sprintf("Cost for q: %.2f", cost_val)
+    # get reactive values
+    alpha <- input$alpha_cost
+    beta <- input$beta_cost
+    w <- input$w
+    r <- input$r
+    q <- input$q
+    # calculate cost
+    cost_q = C(alpha, beta, w, r, q)
+    cost_text <- sprintf("Cost for q=%.2f: %.2f", q, cost_q)
     withMathJax(tags$p(cost_text))
   })
   
-  output$alpha_beta_prod <- renderUI({
-    text <- sprintf("$$\\alpha_{prod}=%.2f, \\beta_{prod}=%.2f$$", input$alpha_prod, input$beta_prod)
-    withMathJax(
-      tags$p(text)
-    )
+  output$alpha_beta_cost <- renderUI({
+    text <- sprintf("$$\\alpha_{cost}=%.2f, \\beta_{cost}=%.2f$$", input$alpha_cost, input$beta_cost)
+    withMathJax(tags$p(text))
   })
   
   output$total_production <- renderUI({
-    total_prod = input$A * (input$L^input$alpha_prod) * (input$K^input$beta_prod)
-    
-    total_prod_text <- sprintf("Total Production (Y): %.2f", total_prod)
-    
-    withMathJax(tags$p(total_prod_text))
+    # get reactive values
+    A <- input$A
+    alpha <- input$alpha_prod
+    beta <- input$beta_prod
+    L <- input$L
+    K <- input$K
+    # calculate total production
+    total_production = Cobb_Douglas(A, alpha, beta, L, K)
+    total_production_text <- sprintf("Total Production: %.2f", total_production)
+    withMathJax(tags$p(total_production_text))
   })
+  
+  output$alpha_beta_prod <- renderUI({
+    text <- sprintf("$$\\alpha_{util}=%.2f, \\beta_{util}=%.2f$$", input$alpha_prod, input$beta_prod)
+    withMathJax(tags$p(text))
+  })
+  
+  # Compute the returns to scale
+  output$returns_to_scale <- renderUI({
+    if (total_prod() > 1) {
+      text <- "Increasing returns to scale"
+    } else if (total_prod() < 1) {
+      text <- "Decreasing returns to scale"
+    } else {
+      text <- "Constant returns to scale"
+    }
+    withMathJax(tags$p(text))
+  })
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
