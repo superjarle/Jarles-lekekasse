8549b8fd44aea0e5cfa7836c247d8fed6deb8ae6
Jarle Kvile <jkv@ssb.no>
2023-07-27 10:40:33 +0200

    Update app.R

--- Shiny/cobb-douglas-econ/app.R
+++ Shiny/cobb-douglas-econ/app.R
@@ -9,106 +9,151 @@
 
 library(shiny)
 library(plotly)
+library(Deriv)
 
-# Define the function outside of the ui and server
-C <- function(a, b, w, r, q, total){
-  (((a/b)^(b/total) + (a/b)^(-a/total)) * w^(a/total) * r^(b/total) * q^(1/total))
+C <- function(alpha, beta, w, r, q){
+  (((alpha/beta)^(beta/alpha) + (alpha/beta)^(-alpha/beta)) * w^(alpha/beta) * r^(beta/alpha) * q^(1/(alpha+beta)))
+}
+
+# Define the Cobb-Douglas production function
+Cobb_Douglas <- function(A, alpha, beta, L, K){
+  A * L^alpha * K^beta
 }
 
 # Define UI
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Cobb-Douglas Cost Functions"),
+  titlePanel("Cobb-Douglas Cost and Production Functions"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      h4("Cost Function Parameters"),
-      p("Set the parameters below for the production function"),
-      helpText("$$q=l^{\alpha}k^{\beta}$$"),
-      # Input: Slider for f ----
-      sliderInput(inputId = "alpha",
-                  label = "Fixed Costs, alpha",
-                  min = 0,
-                  max = 2,
-                  value = 0.5, step=0.25),
-      
-      # Input: Slider for a ----
-      sliderInput(inputId = "beta",
-                  label = "Scale Parameter, beta",
-                  min = 0,
-                  max = 2,
-                  value = 0.5, step=0.25),
-      
-      # Input: Slider for b ----
-      sliderInput(inputId = "w",
-                  label = "Price of labor, w",
-                  min = 0.0,
-                  max = 2.5,
-                  value = 1.5, step=0.5),
-      
-      # Input: Slider for b ----
-      sliderInput(inputId = "r",
-                  label = "Price of capital, r",
-                  min = 0.0,
-                  max = 2.5,
-                  value = 1.5, step=0.5)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotlyOutput("plot"),
-      p("Wage", uiOutput("wage"), align="center"),
-      p("Alpha and Beta", uiOutput("alpha_beta"), align="center"),
-      p("This model is coded by",
-        a("Jarle Kvile",
-          href="https://github.com/superjarle"))
-    )
+  # Define tabs for different sections
+  tabsetPanel(
+    tabPanel("Cost Function", 
+             sidebarLayout(
+               sidebarPanel(
+                 h4("Cost Function Parameters"),
+                 p("Set the parameters below for the production function"),
+                 helpText("$$q=l^{\alpha}k^{\beta}$$"),
+                 sliderInput(inputId = "alpha_cost",
+                             label = "Fixed Costs, alpha",
+                             min = 0,
+                             max = 2,
+                             value = 0.5, step=0.25),
+                 sliderInput(inputId = "beta_cost",
+                             label = "Scale Parameter, beta",
+                             min = 0,
+                             max = 2,
+                             value = 0.5, step=0.25),
+                 sliderInput(inputId = "w",
+                             label = "Price of labor, w",
+                             min = 0.0,
+                             max = 2.5,
+                             value = 1.5, step=0.5),
+                 sliderInput(inputId = "r",
+                             label = "Price of capital, r",
+                             min = 0.0,
+                             max = 2.5,
+                             value = 1.5, step=0.5),
+                 sliderInput(inputId = "q",
+                             label = "Output, q",
+                             min = 0,
+                             max = 100,
+                             value = 50, step=5)
+               ),
+               mainPanel(
+                 plotlyOutput("cost_plot"),
+                 p("Cost for q", uiOutput("cost_q"), align="center"),
+                 p("This model is coded by",
+                   a("Jarle Kvile",
+                     href="https://github.com/superjarle"))
+               )
+             )),
+    tabPanel("Production Function",
+             sidebarLayout(
+               sidebarPanel(
+                 h4("Production Function Parameters"),
+                 p("Set the parameters below for the production function"),
+                 helpText("$$Y=A*L^{\alpha}*K^{\beta}$$"),
+                 sliderInput(inputId = "A",
+                             label = "Total factor productivity, A",
+                             min = 0,
+                             max = 2,
+                             value = 1, step=0.25),
+                 sliderInput(inputId = "alpha_prod",
+                             label = "Output elasticity of labour, alpha",
+                             min = 0,
+                             max = 1,
+                             value = 0.5, step=0.1),
+                 sliderInput(inputId = "beta_prod",
+                             label = "Output elasticity of capital, beta",
+                             min = 0,
+                             max = 1,
+                             value = 0.5, step=0.1),
+                 sliderInput(inputId = "L",
+                             label = "Labour input, L",
+                             min = 0,
+                             max = 100,
+                             value = 50, step=5),
+                 sliderInput(inputId = "K",
+                             label = "Capital input, K",
+                             min = 0,
+                             max = 100,
+                             value = 50, step=5)
+               ),
+               mainPanel(
+                 plotlyOutput("prodity_plot"),
+                 p("Total Production (Y)", uiOutput("total_production"), align="center"),
+                 p("Alpha and Beta for Production Function", uiOutput("alpha_beta_prod"), align="center")
+               )
+             ))
   )
 )  
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  # Calculate total here
-  total <- reactive({
-    input$alpha + input$beta
-  })
-  
-  output$plot <- renderPlotly({
-    
-    # total cost function
-    C_func <- function(q){C(input$alpha, input$beta, input$w, input$r, q, total())}
-    
+  output$cost_plot <- renderPlotly({
+    C_func <- function(q){C(input$alpha_cost, input$beta_cost, input$w, input$r, q)}
     df <- data.frame(x = seq(0,10, length.out = 1000))
     df$y <- C_func(df$x)
-    
     plot_ly(df, x = ~x, y = ~y, type = 'scatter', mode = 'lines') %>%
       layout(title = "Cost Function", 
              xaxis = list(title = "Output (q)"),
              yaxis = list(title = "Costs"))
   })
   
-  output$wage<-renderUI({
-    w <- reactive(input$w)
-    FC <- "$$wage=%.0f$$"
-    text <- sprintf(FC, w())
-    withMathJax(  
-      tags$p(text)
-    )
+  output$prodity_plot <- renderPlotly({
+    Cobb_func <- function(L){Cobb_Douglas(input$A, input$alpha_prod, input$beta_prod, L, input$K)}
+    df <- data.frame(x = seq(0,10, length.out = 1000))
+    df$y <- Cobb_func(df$x)
+    plot_ly(df, x = ~x, y = ~y, type = 'scatter', mode = 'lines') %>%
+      layout(title = "Production Function", 
+             xaxis = list(title = "Labour Input (L)"),
+             yaxis = list(title = "Total Production (Y)"))
   })
   
-  output$alpha_beta <- renderUI({
-    alpha <- reactive(input$alpha)
-    beta <- reactive(input$beta)
-    text <- sprintf("$$\\alpha=%.2f, \\beta=%.2f$$", alpha(), beta())
+  output$cost_q <- renderUI({
+    cost_val = C(input$alpha_cost, input$beta_cost, input$w, input$r, input$q)
+    cost_text <- sprintf("Cost for q: %.2f", cost_val)
+    withMathJax(tags$p(cost_text))
+  })
+  
+  output$alpha_beta_prod <- renderUI({
+    text <- sprintf("$$\\alpha_{prod}=%.2f, \\beta_{prod}=%.2f$$", input$alpha_prod, input$beta_prod)
     withMathJax(
       tags$p(text)
     )
   })
   
+  output$total_production <- renderUI({
+    total_prod = input$A * (input$L^input$alpha_prod) * (input$K^input$beta_prod)
+    
+    total_prod_text <- sprintf("Total Production (Y): %.2f", total_prod)
+    
+    withMathJax(tags$p(total_prod_text))
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
