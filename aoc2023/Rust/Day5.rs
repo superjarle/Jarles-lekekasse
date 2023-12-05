27660a435c656b2fc072d0991d0ef300cba741b0
Jarle Kvile <jkv@ssb.no>
2023-12-05 10:59:55 +0100

    Rust day 5

--- aoc2023/Rust/Day5.rs
+++ aoc2023/Rust/Day5.rs
@@ -1,43 +1,49 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::error::Error;
 
-fn make_maps(data: Vec<String>) -> Vec<(i32, i32, i32)> {
-    let mut maps: Vec<(i32, i32, i32)> = data.iter().map(|line| {
-        let parts: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
-        (parts[1], parts[1] + parts[2], parts[0])
+fn make_maps(data: &[String]) -> Vec<(i32, i32, i32)> {
+    let mut maps: Vec<_> = data.iter().filter_map(|line| {
+        let parts: Vec<i32> = line.split_whitespace()
+                                  .filter_map(|s| s.parse().ok())
+                                  .collect();
+        if parts.len() == 3 {
+            Some((parts[1], parts[1] + parts[2], parts[0]))
+        } else {
+            None
+        }
     }).collect();
-    maps.sort();
+
+    maps.sort_unstable();
     maps
 }
 
-fn convert(value: i32, maps: &Vec<(i32, i32, i32)>) -> i32 {
-    for map in maps {
-        let (src_start, src_end, dest_start) = map;
-        if *src_start <= value && value < *src_end {
-            return value - src_start + dest_start;
-        }
-    }
-    value
+fn convert(value: i32, maps: &[(i32, i32, i32)]) -> i32 {
+    maps.iter()
+        .find(|&&(src_start, src_end, _)| src_start <= value && value < src_end)
+        .map_or(value, |&(src_start, _, dest_start)| value - src_start + dest_start)
 }
 
-fn read_from_file(filename: &Path) -> (Vec<(i32, i32)>, Vec<Vec<String>>) {
-    let file = File::open(filename).unwrap();
-    let lines: Vec<String> = io::BufReader::new(file).lines().map(|l| l.unwrap()).collect();
+fn read_from_file(filename: &Path) -> Result<(Vec<(i32, i32)>, Vec<Vec<String>>), Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let lines: Vec<String> = io::BufReader::new(file).lines().collect::<Result<_, _>>()?;
 
-    let seeds_line = &lines[0];
-    let seeds_str = seeds_line.split(": ").nth(1).unwrap();
-    let seeds: Vec<(i32, i32)> = seeds_str.split_whitespace().map(|s| s.parse().unwrap())
-                                         .collect::<Vec<i32>>().chunks(2)
-                                         .map(|chunk| (chunk[0], chunk[1])).collect();
+    let seeds = lines.get(0).ok_or("No seed line found")?
+                      .split(": ").nth(1).ok_or("Incorrect seed line format")?
+                      .split_whitespace()
+                      .collect::<Vec<_>>()
+                      .chunks(2)
+                      .filter_map(|chunk| Some((chunk.get(0)?.parse().ok()?, chunk.get(1)?.parse().ok()?)))
+                      .collect();
 
     let mut stages = Vec::new();
     let mut stage = Vec::new();
-    for line in &lines[1..] {
+    for line in lines.iter().skip(1) {
         if line.ends_with(" map:") {
             if !stage.is_empty() {
-                stages.push(stage);
-                stage = Vec::new();
+                stages.push(stage.clone());
+                stage.clear();
             }
         } else if !line.is_empty() {
             stage.push(line.clone());
@@ -47,32 +53,28 @@ fn read_from_file(filename: &Path) -> (Vec<(i32, i32)>, Vec<Vec<String>>) {
         stages.push(stage);
     }
 
-    (seeds, stages)
+    Ok((seeds, stages))
 }
 
-fn lowest_location(filename: &str) -> i32 {
+fn lowest_location(filename: &str) -> Result<i32, Box<dyn Error>> {
     let path = Path::new(filename);
-    let (seeds, data) = read_from_file(&path);
+    let (seeds, data) = read_from_file(&path)?;
 
-    let mut maps_list = Vec::new();
-    for stage in data {
-        maps_list.push(make_maps(stage));
-    }
+    let maps_list: Vec<_> = data.iter().map(|stage| make_maps(stage)).collect();
 
-    let mut min_locations = Vec::new();
-    for seed in seeds {
-        let mut location = seed.0;
-        for maps in &maps_list {
-            location = convert(location, maps);
-        }
-        min_locations.push(location);
-    }
+    let min_location = seeds.iter()
+                            .map(|&(start, _)| maps_list.iter().fold(start, |location, maps| convert(location, maps)))
+                            .min()
+                            .ok_or("No minimum location found")?;
 
-    *min_locations.iter().min().unwrap()
+    Ok(min_location)
 }
 
 fn main() {
     let filename = "day5.txt";
-    let result = lowest_location(filename);
-    println!("Lowest location: {}", result);
+
+    match lowest_location(filename) {
+        Ok(result) => println!("Lowest location: {}", result),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
